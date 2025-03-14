3287c06cb3d09ab564d9b8c77ac9dba68fd2f5a2
Raido Paas <raidop135@gmail.com>
2025-03-14 12:37:02 +0200

    updated wsgi

--- raiisite/raiisite/wsgi.py
+++ raiisite/raiisite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raiisite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raiisite.raiisite.settings')
 
 application = get_wsgi_application()
